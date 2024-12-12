dacf57a5fcace4669c16b649b7ffbd3841af1c9f
Nikhil <nikhil.kumar@cesltd.com>
2024-12-12 14:24:49 +0530

    fix:upadted the scripts according to the PR comments

--- tests/signInPageTests.spec.js
+++ tests/signInPageTests.spec.js
@@ -2,22 +2,23 @@ import { test } from '@playwright/test'
 import HomePage from '../PageObjects/homePageFns'
 import Verify from '../Utils/Assertions/verify'
 import Click from '../Utils/Interaction/click'
-import pageSels from '../Utils/Selectors/pageSels'
+import PageSels from '../Utils/Selectors/pageSels'
 
-const { skipBtnText } = pageSels.signInPage
-const { signInBtn, signIn } = pageSels.homePage
+const { skipBtnText } = PageSels.signInPage
+const { signInBtn, signIn } = PageSels.homePage
 
-let click, verify, homePage, urlCheck
-test.describe('Should execute KFC India test scenarios- Sign In page', () => {
+let click, verify, homePage, urlCheck, baseURL
+test.describe('Should execute KFC India test scenarios - Sign In page', () => {
     test.beforeEach('It should navigate to home page before each test block', async ({ page }) => {
         homePage = new HomePage(page)
         verify = new Verify(page)
         click = new Click(page)
+        baseURL = page.context()._options.baseURL
 
         //navigating to home page and verifying the URL
         await homePage.navigateToHomePage('/')
         urlCheck = await verify.theUrl()
-        await urlCheck.equalsTo('https://online.kfc.co.in/');
+        await urlCheck.equalsTo(baseURL);
 
     })
 
@@ -28,7 +29,7 @@ test.describe('Should execute KFC India test scenarios- Sign In page', () => {
 
         //verifying the sign is skipped
         urlCheck = await verify.theUrl()
-        await urlCheck.equalsTo('https://online.kfc.co.in/');
+        await urlCheck.equalsTo(baseURL);
 
         //verifying the default sign in text
         const signInBtnText = await verify.theElement(signInBtn)
